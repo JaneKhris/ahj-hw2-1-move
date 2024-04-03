4d044c82385bc41bdd676e08ffaabc8050900b78
Jane Khris <efkhrisanfova@gmail.com>
2024-04-03 12:06:16 +0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Домашнее задание к занятию "2. DOM"
+### Задача "Перемещение элемента"
+
+![CI](https://github.com/JaneKhris/ahj-hw2.1-move/actions/workflows/web.yml/badge.svg)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/71s7mnmd8dkompvy?svg=true)](https://ci.appveyor.com/project/JaneKhris/ahj-hw2-1-move)
+
---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+let activeHole = 1;
+
+function getHole(index) { document.getElementById(`hole${index}`); }
+
+function deactivateHole(index) { getHole(index).className = 'hole'; }
+
+function activateHole(index) { getHole(index).className = 'hole hole_has-mole'; }
+
+setInterval(() => {
+  deactivateHole(activeHole);
+  let r = true;
+  while (r) {
+    const newActiveHole = Math.floor(1 + Math.random() * 16);
+    if (newActiveHole !== activeHole) {
+      r = false;
+      activeHole = newActiveHole;
+    }
+  }
+  activateHole(activeHole);
+}, 800);
